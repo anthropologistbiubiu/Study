91f1513b0dc22de7eac683b1f7c6d730e09c7a51
anthropologistbiubiu <1319847957@qq.com>
2025-02-07 23:34:36 +0800

    study

--- basic/onion_model/onion_model.go
+++ basic/onion_model/onion_model.go
@@ -1,12 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 type HTTPMiddlerware func(handler http.Handler) http.Handler
 
-func HTTPChain(opt ...HTTPMiddlerware) HTTPMiddlerware {
-	return nil
+func HTTPChain(opts ...HTTPMiddlerware) HTTPMiddlerware {
+	return func(next http.Handler) http.Handler {
+		for i := len(opts) - 1; i >= 0; i-- {
+			next = opts[i](next)
+		}
+		return next
+	}
 }
 
+func LoggerMiddler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(r http.ResponseWriter, w *http.Request) {
+		log.Println("befor handler")
+		next.ServeHTTP(r, w)
+		log.Println("afeter handler")
+	})
+}
 
-func
+type Router struct {
+	routers map[string]string
+}
+
+func (router *Router) ServeHTTP(r http.ResponseWriter, w *http.Request) {
+
+}
+
